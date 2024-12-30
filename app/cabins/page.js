18c7667c1bc1dff602f341eba43f69d6b253ff26
Grant626 <grant626f@gmail.com>
2024-12-30 16:33:44 -0500

    context for date picker state management

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import CabinList from '../_components/CabinList';
 import { Suspense } from 'react';
 import Spinner from '../_components/Spinner';
 import Filter from '../_components/Filter';
+import ReservationReminder from '../_components/ReservationReminder';
 
 //OPT OUT OF DATA CACHE (ROUTE LEVEL) || SET INTERVAL (SECS)
 //ONLY relevant to static pages, after filter page is now dynamic
@@ -35,6 +36,7 @@ export default function Page({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
